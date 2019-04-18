3c50a4c481859f8ca698d5b2ae4ea04bff3b4d87
Jose Miguel Angel Amaro Hernandez <jma_hernandez@hotmail.com>
2019-04-18 16:29:40 -0500

    fix tag names

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
-docker build -t jmahernandez/multi-client:latest -t jmahernandez/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t jmahernandez/multi-server:latest -t jmahernandez/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t jmahernandez/multi-worker:latest -t jmahernandez/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t jmahernandez/multi_client:latest -t jmahernandez/multi_client:$SHA -f ./client/Dockerfile ./client
+docker build -t jmahernandez/multi_server:latest -t jmahernandez/multi_server:$SHA -f ./server/Dockerfile ./server
+docker build -t jmahernandez/multi_worker:latest -t jmahernandez/multi_worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push jmahernandez/multi-client:latest
-docker push jmahernandez/multi-server:latest
-docker push jmahernandez/multi-worker:latest
+docker push jmahernandez/multi_client:latest
+docker push jmahernandez/multi_server:latest
+docker push jmahernandez/multi_worker:latest
 
-docker push jmahernandez/multi-client:$SHA
-docker push jmahernandez/multi-server:$SHA
-docker push jmahernandez/multi-worker:$SHA
+docker push jmahernandez/multi_client:$SHA
+docker push jmahernandez/multi_server:$SHA
+docker push jmahernandez/multi_worker:$SHA
 
 kubectl apply -f k8s
 
-kubectl set image deployments/client-deployment client=jmahernandez/multi-client:$SHA
-kubectl set image deployments/server-deployment server=jmahernandez/multi-server:$SHA
-kubectl set image deployments/worker-deployment worker=jmahernandez/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=jmahernandez/multi_client:$SHA
+kubectl set image deployments/server-deployment server=jmahernandez/multi_server:$SHA
+kubectl set image deployments/worker-deployment worker=jmahernandez/multi_worker:$SHA
