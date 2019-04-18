e8f7783f58a2512c472e31c3916eb9b8c3363e3b
Jose Miguel Angel Amaro Hernandez <jma_hernandez@hotmail.com>
2019-04-18 16:09:26 -0500

    fix repository name for travis

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
-docker build -t jmahernandez/multi-client:latest -t jmahernandez/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t jmahernandez/multi-server:latest -t jmahernandez/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t jmahernandez/multi-worker:latest -t jmahernandez/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t jmaHernandez/multi-client:latest -t jmaHernandez/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jmaHernandez/multi-server:latest -t jmaHernandez/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jmaHernandez/multi-worker:latest -t jmaHernandez/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push jmahernandez/multi-client:latest
-docker push jmahernandez/multi-server:latest
-docker push jmahernandez/multi-worker:latest
+docker push jmaHernandez/multi-client:latest
+docker push jmaHernandez/multi-server:latest
+docker push jmaHernandez/multi-worker:latest
 
-docker push jmahernandez/multi-client:$SHA
-docker push jmahernandez/multi-server:$SHA
-docker push jmahernandez/multi-worker:$SHA
+docker push jmaHernandez/multi-client:$SHA
+docker push jmaHernandez/multi-server:$SHA
+docker push jmaHernandez/multi-worker:$SHA
 
 kubectl apply -f k8s
 
-kubectl set image deployments/client-deployment client=jmahernandez/multi-client:$SHA
-kubectl set image deployments/server-deployment server=jmahernandez/multi-server:$SHA
-kubectl set image deployments/worker-deployment worker=jmahernandez/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=jmaHernandez/multi-client:$SHA
+kubectl set image deployments/server-deployment server=jmaHernandez/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=jmaHernandez/multi-worker:$SHA
